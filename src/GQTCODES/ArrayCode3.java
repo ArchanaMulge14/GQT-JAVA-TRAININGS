64a9f80a9733443bd29e6c7ce7a980be4eb88518
Archana R Mulge <Archana R Mulge@Archana>
2023-04-07 19:59:37 +0530

    Array3Commit

---
+++ src/GQTCODES/ArrayCode3.java
@@ -0,0 +1,11 @@
+package GQTCODES;
+/*
+ * (i)         (j)          (k)
+ * School      class        Students
+ * 0             5
+ * 1             5
+ * 2             5
+ */
+public class ArrayCode3 {
+
+}
